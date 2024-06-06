b980fff33f4171a906da8ccae8ec948d9d64f4c5
arafatGithubCode <md489288@gmail.com>
2024-06-06 15:30:12 +0600

    Layouts

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "lightcyan",
+        border: "1px solid red",
+        padding: "1rem",
+      }}
+    >
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/layout/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header
+      style={{
+        backgroundColor: "lightgray",
+        border: "1px solid red",
+        padding: "1rem",
+      }}
+    >
+      <p>Header</p>
+    </header>
+  );
+};
+
+export default Header;

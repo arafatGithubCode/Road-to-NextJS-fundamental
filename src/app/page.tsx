3f7823b773d89f258788878883adeb3769d739f0
arafatGithubCode <md489288@gmail.com>
2024-06-06 18:05:16 +0600

    Link Component Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div>
       <h1>welcome to home!</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">-products</Link>
     </div>
   );
 };

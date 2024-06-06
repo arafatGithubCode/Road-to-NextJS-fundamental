26650951b2f78c21877c68d4a4c68f3ab6be60d6
arafatGithubCode <md489288@gmail.com>
2024-06-06 17:50:05 +0600

    title Metadata

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "first blog",
+  },
+};
 const First = () => {
   return <h2>First blog page (Nested Routing)</h2>;
 };
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog",
+};
+
 const Blog = () => {
   return <h2>Blog page (Nested Routing)</h2>;
 };

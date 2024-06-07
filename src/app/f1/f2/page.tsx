980951d9d2427a872ae0b622eb48e7c5ac88b918
arafatGithubCode <md489288@gmail.com>
2024-06-07 21:17:36 +0600

    Intercepted route

---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function f2() {
+  return (
+    <>
+      <h2>f2 page</h2>
+      <div></div>
+    </>
+  );
+}
---
+++ src/app/f1/f4/(..)f3/page.tsx
@@ -0,0 +1,3 @@
+export default function F3() {
+  return <h2>(..) Intercepted F3 page</h2>;
+}

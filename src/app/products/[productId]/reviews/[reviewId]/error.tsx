b60f99f8b4a0bd1f55e9b22df52ee5bc186bca97
arafatGithubCode <md489288@gmail.com>
2024-06-07 11:33:24 +0600

    Recovering from Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,15 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <h3>{error.message}</h3>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <h3>
+      {error.message} <button onClick={reset}>try again</button>
+    </h3>
+  );
 }

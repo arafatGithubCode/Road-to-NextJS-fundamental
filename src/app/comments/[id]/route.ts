03b3a3374a9970d882b549fb9bdac4295aeaaa11
arafatGithubCode <md489288@gmail.com>
2024-06-08 15:42:43 +0600

    NextRequest

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,34 +1,12 @@
+import { NextRequest } from "next/server";
 import { comments } from "../data";
 
-export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } }
-) {
-  const comment = comments.find((c) => c.id === parseInt(params.id));
-  return Response.json(comment);
-}
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
 
-export async function PATCH(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const body = await request.json();
-  const { text } = body;
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
-  comments[index].text = text;
-  return Response.json(comments[index]);
-}
-
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const id = parseInt(params.id);
-  const index = comments.findIndex((comment) => comment.id === id);
-  const deletedComment = comments[index];
-  comments.splice(index, 1);
-
-  return Response.json(deletedComment);
+  return Response.json(filteredComments);
 }

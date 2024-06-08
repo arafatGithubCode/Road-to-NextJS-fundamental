e351c7a497d00f15768a7c8d7c6868560f32a1fc
arafatGithubCode <md489288@gmail.com>
2024-06-08 11:32:55 +0600

    Dynamic Route Handlers

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,9 @@
+import { comments } from "../data";
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const comment = comments.find((c) => c.id === parseInt(params.id));
+  return Response.json(comment);
+}

f610100cc78e35fee87633b38a2f7f2fd4322eb5
arafatGithubCode <md489288@gmail.com>
2024-06-10 09:12:36 +0600

    Caching Data

---
+++ src/app/products/page.tsx
@@ -0,0 +1,24 @@
+type Product = {
+  id: number;
+  price: number;
+  title: string;
+  description: string;
+};
+export default async function page() {
+  const response = await fetch("http://localhost:3001/products");
+  const products = await response.json();
+  return (
+    <div className="flex gap-5">
+      {products &&
+        products.map((product: Product) => {
+          return (
+            <div className="m-4 p-5 shadow-lg" key={product.id}>
+              <h3>{product.title}</h3>
+              <p>{product.price}</p>
+              <p>{product.description}</p>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
